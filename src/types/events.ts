f1eea8a172c49c8d742997885214c2e506a03af4
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-11-10 16:03:05 -0300

    feat: update events hanlders

--- src/types/events.ts
+++ src/types/events.ts
@@ -9,13 +9,13 @@ export interface ServerEvents {
     "room:created": (code: number) => void;
     "room:join-player": (player: Player) => void;
     "room:error": (message: string) => void;
+    "room:room-exists": (exists: boolean) => void;
     "player:joined-room": (player: Player) => void;
-    "player:adds-name": (player: Player) => void;
 }
 
 export interface ClientEvents {
     hello: () => void;
     "room:create": () => void;
-    "player:join-room": (code: number) => void;
-    "player:add-name": (player: Player, code: number) => void;
+    "room:check-exists": (code: number) => void;
+    "player:join-room": (code: number, playername: string) => void;
 }
