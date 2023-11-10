70a50ffdeab81f57b07cdb1c9af6fbf6e683ad5c
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-11-10 16:59:56 -0300

    feat: send event for start quiz to all players

--- src/listeners/game.handlers.ts
+++ src/listeners/game.handlers.ts
@@ -14,9 +14,16 @@ export default function (
     const startGame = () => {
         const roomFound = rooms.find((r) => r.code == socket.data.code);
 
+        // verifico que se encuentre la sala
         if (roomFound && socket.data.role === "moderator") {
-            roomFound.status = "started";
-            socket.emit("quiz:started");
+            roomFound.status = "started"; // etsablezco el estado de la sala en "started"
+
+            socket.emit("quiz:started"); // envio el evento que se ha iniciado el juego al cliente
+
+            // emito el evento que se ha iniciado el juego a todos los jugadores
+            roomFound.players.forEach((p) => {
+                socket.to(p.socketId).emit("quiz:started");
+            });
         }
     };
 
