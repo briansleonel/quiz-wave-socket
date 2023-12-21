7717bf88f63e72b2e3b6f9851fe86c2c4edd292f
briansleonel <gonzalezbrianleonel76@gmail.com>
2023-12-21 16:13:31 -0300

    feat: add event listener to show question and update countdown

--- src/listeners/game.handlers.ts
+++ src/listeners/game.handlers.ts
@@ -17,15 +17,53 @@ export default function (
         // verifico que se encuentre la sala
         if (roomFound && socket.data.role === "moderator") {
             roomFound.status = "started"; // etsablezco el estado de la sala en "started"
-
-            socket.emit("quiz:started"); // envio el evento que se ha iniciado el juego al cliente
+            roomFound.currentQuestion = 1;
 
             // emito el evento que se ha iniciado el juego a todos los jugadores
             roomFound.players.forEach((p) => {
-                socket.to(p.socketId).emit("quiz:started");
+                socket
+                    .to(p.socketId)
+                    //.emit("quiz:show-question", currentQuestion, hasNext);
+                    .emit("quiz:started");
+                //socket.to(p.socketId).emit("quiz:started");
+            });
+
+            socket.emit("quiz:started"); // envio el evento que se ha iniciado el juego al cliente
+        }
+    };
+
+    const quizShowQuestion = () => {
+        const roomFound = rooms.find((r) => r.code == socket.data.code);
+
+        // verifico que se encuentre la sala
+        if (roomFound && socket.data.role === "moderator") {
+            const currentQuestion =
+                roomFound.questions[roomFound.currentQuestion];
+            const hasNext =
+                roomFound.currentQuestion + 1 < roomFound.questions.length;
+
+            // emito el evento que se ha iniciado el juego a todos los jugadores
+            roomFound.players.forEach((p) => {
+                socket
+                    .to(p.socketId)
+                    .emit("quiz:show-question", currentQuestion.question);
+            });
+        }
+    };
+
+    const changeCountdown = (count: number) => {
+        const roomFound = rooms.find((r) => r.code == socket.data.code);
+
+        // verifico que se encuentre la sala
+        if (roomFound && socket.data.role === "moderator") {
+            // emito el evento que se ha iniciado el juego a todos los jugadores
+            roomFound.players.forEach((p) => {
+                socket.to(p.socketId).emit("quiz:countdown", count);
             });
         }
     };
 
     socket.on("quiz:start", startGame);
+    socket.on("quiz:show-question", quizShowQuestion);
+    socket.on("quiz:countdown", changeCountdown);
 }
