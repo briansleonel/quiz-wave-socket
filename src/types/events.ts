566e038e5eef9eae534041f246e7ce08ac2af935
briansleonel <gonzalezbrianleonel76@gmail.com>
2024-04-08 23:37:43 -0300

    refractor: change events to create a room

--- src/types/events.ts
+++ src/types/events.ts
@@ -11,7 +11,11 @@ export interface SocketData {
 
 export interface ServerEvents {
     response: (message: string) => void;
-    "room:created": (code: number, socketId: string) => void;
+    "room:created": (
+        code: number,
+        socketId: string,
+        questions: Array<ICollectionQuestion>
+    ) => void;
     "room:join-player": (player: Player) => void;
     "room:error": (message: string) => void;
     "room:room-exists": (exists: boolean) => void;
@@ -32,7 +36,7 @@ export interface ServerEvents {
 
 export interface ClientEvents {
     hello: () => void;
-    "room:create": (questions: Array<ICollectionQuestion>) => void;
+    "room:create": (id: string) => void;
     "room:check-exists": (code: number) => void;
     "room:close-room": (code: number) => void;
     "player:join-room": (code: number, playername: string) => void;
